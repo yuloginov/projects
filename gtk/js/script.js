249b5ff84a52e916775931041d256f5aa2a5db56
George <yu.loginov@gmail.com>
2014-07-30 20:41:37 +0400

    30 07 evening

---
+++ gtk/js/script.js
@@ -0,0 +1,9 @@
+$(function() {
+	$('.parralax .element').imageScroll({
+		image: 'content/parralax.png',
+		container: $('.parralax'),
+		coverRatio: 0.38,
+		speed: 0.0,
+		forcedimageheight: 322
+	});
+});
